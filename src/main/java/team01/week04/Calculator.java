f4bc74bb38b82729cd3541ea084de4f201b888ca
JSoi <jsoi950715@gmail.com>
2016-04-09 15:33:02 +0900

    delete outline command

--- src/main/java/team01/week04/Calculator.java
+++ src/main/java/team01/week04/Calculator.java
@@ -78,7 +78,6 @@ public class Calculator {
 		        cal.time= Integer.parseInt(fileline.split(" ")[1]);
 		        cal.line= Integer.parseInt(fileline.split(" ")[2]);
 			logInfo.info(String.format("%.2f",cal.money()));
-			//logInfo.info(String.format("%.2f",cal1.money()));
 			logInfo.info("\n");
 		}
 		br.close();
